bda4d275fb3faf9a0b8544335e3ff2f7b55fe7ba
ThomasConnolly <tconnolly@embarqmail.com>
2013-09-03 14:16:06 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EfM::Application.config.secret_key_base = 'e8e3fc7499e8c85df29596228df41a166fece7d9cfa6731405de916d0eb2a751e570378bfad5ff1c40a022316c7e983ae9c3ca3f052d505b6b8a1d898be374d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EfM::Application.config.session_store :cookie_store, key: '_EfM_session'
